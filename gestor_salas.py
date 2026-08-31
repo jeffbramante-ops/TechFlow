6b8989c893183c83c49773bb9f0c82f0ae9a78be
jeffbramante-ops <jeffbramante@gmail.com>
2026-08-31 13:01:47 +0000

    etapa de cadastro

--- gestor_salas.py
+++ gestor_salas.py
@@ -1,1 +1,10 @@
+#Etapa1 -Cadastro das Empresas e seus projetos
+startups = {
+    "nome": "flare solution",
+    "etapa": "aceleração",
+    "inicio": "abril de 2026"
+}
+projetos_ativos =["app mobile", "signal found"]
 
+print("STARTUP:",startups["nome"],"fase:",startups["etapa"])
+print("projeto em andamento:",projetos_ativos[0],"inicio:",startups["inicio"])
