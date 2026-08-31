4d7e5ad744b967396a1612ca1330b2dca8fa9e50
jeffbramante-ops <jeffbramante@gmail.com>
2026-08-31 14:11:05 +0000

    etapa de mapeamento

--- gestor_salas.py
+++ gestor_salas.py
@@ -8,3 +8,15 @@ projetos_ativos =["app mobile", "signal found"]
 
 print("STARTUP:",startups["nome"],"fase:",startups["etapa"])
 print("projeto em andamento:",projetos_ativos[0],"inicio:",startups["inicio"])
+
+#etapa2 -mapeamento de salas
+#sala ocupada = 1 e sala livre =0
+salas = [
+    [1,0],
+    [0,1]
+]
+print("status da sala A1", salas[0][0])
+print("status da sala A2", salas[0][1])
+print("status da sala B1", salas[1][0])
+print ("status da sala B2", salas[1][1])
+print("classificação de status: 1= ocupado e 0 = livre")
