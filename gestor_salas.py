7a6ccf629d6ba058e6609a4effa5d456b652f860
jeffbramante-ops <jeffbramante@gmail.com>
2026-08-31 16:00:58 +0000

    leitura de dados

--- gestor_salas.py
+++ gestor_salas.py
@@ -20,3 +20,16 @@ print("status da sala A2", salas[0][1])
 print("status da sala B1", salas[1][0])
 print ("status da sala B2", salas[1][1])
 print("classificação de status: 1= ocupado e 0 = livre")
+#etapa3 -leitura de dados operacionais
+with open("dados.csv", "r", encoding="utf-8") as arquivo:
+    cabecalho = arquivo.readline()
+    linha1 = arquivo.readline()
+    linha2 = arquivo.readline()
+    linha3 = arquivo.readline()
+    linha4 = arquivo.readline()
+
+print("cabealho:", cabecalho)
+print("linha1:", linha1)
+print("linha2:", linha2)
+print("linha3:", linha3)
+print("linha4:", linha4)
